eab0518ada720a75a976e3ae9efcb9862822733b
Samsegun <oyebadesamsegun@gmail.com>
2024-01-25 22:27:01 +0100

    encapsulation and abstraction

--- src/com/moshoop/Employee.java
+++ src/com/moshoop/Employee.java
@@ -3,28 +3,46 @@ package com.moshoop;
 public class Employee {
     private int baseSalary;
     private int hourlyRate;
+    public static int numOfEmployees;
 
-    public void setHourlyRate(int hourlyRate) {
+    public Employee() {
+        this(10_000, 20);
+    }
+    public Employee(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+
+        numOfEmployees++;
+    }
+
+    private void setHourlyRate(int hourlyRate) {
         if(hourlyRate <= 0)
             throw new IllegalArgumentException("Hourly rate cannot be zero or less");
         this.hourlyRate = hourlyRate;
     }
 
-    public void setBaseSalary(int baseSalary) {
+    private void setBaseSalary(int baseSalary) {
         if(baseSalary <= 0)
             throw new IllegalArgumentException("Salary cannot be zero or less");
         this.baseSalary = baseSalary;
     }
 
-    public int getBaseSalary() {
+    private int getBaseSalary() {
         return baseSalary;
     }
 
-    public int getHourlyRate() {
+    private int getHourlyRate() {
         return hourlyRate;
     }
 
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
+    }
+    public int calculateWage() {
+        return calculateWage(0);
+    }
+
+    public static void printNumberOfEmployees() {
+        System.out.println("Number of employees: " + numOfEmployees);
     }
 }
--- src/com/moshoop/Main.java
+++ src/com/moshoop/Main.java
@@ -3,10 +3,9 @@ package com.moshoop;
 public class Main {
     public static void main(String[] args) {
         var employee1 = new Employee();
-        employee1.setBaseSalary(10_000);
-        employee1.setHourlyRate(20);
-        System.out.println(employee1.getBaseSalary());
-        System.out.println(employee1.getHourlyRate());
+        var employee2 = new Employee(50_000, 30);
 
+        System.out.println(employee2.calculateWage(10));
+        Employee.printNumberOfEmployees();
     }
 }
