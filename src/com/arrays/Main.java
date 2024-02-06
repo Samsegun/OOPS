80d6d253d4d3a08321e69db37bbdfc1e4041aa1c
Samsegun <oyebadesamsegun@gmail.com>
2024-02-06 22:24:03 +0100

    array exercise1

---
+++ src/com/arrays/Main.java
@@ -0,0 +1,37 @@
+package com.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int arrayLength;
+
+        while(true) {
+            System.out.print("Enter length of array: ");
+            arrayLength = scanner.nextInt();
+
+            if(arrayLength < 1 || arrayLength > 20) {
+                System.out.println("Invalid range. Try a different number!");
+                continue;
+            }
+
+            break;
+        }
+
+
+        int[] numbers = new int[arrayLength];
+        fillAndPrintArray(numbers);
+
+    }
+
+    public static void fillAndPrintArray(int[] numbers) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < numbers.length; i++)
+            numbers[i] = scanner.nextInt();
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
